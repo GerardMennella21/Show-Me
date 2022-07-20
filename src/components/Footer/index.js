e92dc76cad9f9874c737da9f5c05b368ba3d8b19
Gerard Mennella <gerardmennella21@gmail.com>
2022-07-19 20:17:27 -0400

    initial commit

---
+++ src/components/Footer/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Footer() {
+    return (
+        <footer>
+            Made By Gerard Mennella, Dawson Filion, Jonathan Price and Khyan Alverez
+        </footer>
+    )
+}
