07b0d74e9dee4a7528ef8615ceb6fa024c421ec3
Dawson Filion <Dawsonfilion21@gmail.com>
2022-07-22 20:53:40 -0400

    index and comment models

---
+++ server/models/Reaction.js
@@ -0,0 +1,24 @@
+const { Schema } = required('mongoose');
+const dateFormat = required('../utils/dateFormat');
+
+const reactionSchema = new Schema(
+    {
+        reactionBody: {
+            type: String,
+            required: true,
+            maxlength: 280
+        },
+        username:{
+            type: Date,
+            default: Date.now,
+            get: timestamp => dateFormat(timestamp)
+        }
+    },
+    {
+        toJSON: {
+            getters: true
+        }
+    }
+);
+
+module.exports = reactionSchema;
