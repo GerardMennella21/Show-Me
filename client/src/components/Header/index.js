ffeda10be08eaf740e29d0597f262529ba2aca07
Jonathan Price <jonprice0@gmail.com>
2022-07-24 20:42:58 -0400

    added initial header component with a simple nav included

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,9 +1,34 @@
 import React from "react";
+import { Link } from 'react-router-dom';
+import Auth from '../../utils/auth';
 
 export default function Header() {
+    const logout = event => {
+        event.preventDefault();
+        Auth.logout();
+    };
+
     return (
         <header>
-            <h1>Show-Me</h1>
+            <div>
+                <Link to="/">
+                <h1>Show Me</h1>
+                </Link>
+
+                <nav>
+                    {Auth.loggedIn() ? (
+                        <>
+                            <Link to="/profile">My Profile</Link>
+                            <a href="/" onClick={logout}>Logout</a>
+                        </>
+                    ) : (
+                        <>
+                            <Link to="/login">Login</Link>
+                            <Link to="/signup">Signup</Link>
+                        </>
+                    )}
+                </nav>
+            </div>
         </header>
-    )
-}
+    );
+};
