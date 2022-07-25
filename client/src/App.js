ea22e2c7fd4f2ccababeb033e308dbc32dba99cf
Jonathan Price <jonprice0@gmail.com>
2022-07-24 23:01:26 -0400

    added minimum code to get the app component to render from index.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,20 @@
+import React from 'react';
 import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div>
-      <Header />
-      <main>
+    <Router>
+      <div>
+        <Header />
+        <main>
 
-      </main>
-      <Footer />
-    </div>
+        </main>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
