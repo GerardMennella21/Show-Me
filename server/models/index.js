49c7fb91632b0d179aea3f5bf57f9a82f7198ef0
Gerard Mennella <gerardmennella21@gmail.com>
2022-07-26 13:46:58 -0400

    refactor code

--- server/models/index.js
+++ server/models/index.js
@@ -1,4 +1,4 @@
 const User = require('./User');
-const Thought = require('./Comment');
+const Image = require('./Image');
 
-module.exports = { User, Thought};
+module.exports = { User, Image};
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,12 +1,7 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
-type User {
-    _id: ID 
-    username: String
-    email: String
-    friendCount: Int
-    comment: [Comment]
-    friends: [User]
-}
+
 `
+
+module.exports = typeDefs;
