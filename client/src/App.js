9ac8985d6c565adcb175fe64b45e439710278395
Gerard Mennella <gerardmennella21@gmail.com>
2022-07-27 12:58:51 -0400

    Revert "Merge pull request #4 from Dawsonf18/main"
    This reverts commit e09aaa09bedef7d3f14d30415cc637a24fdbbdf1, reversing
    changes made to 8a222316d7adf9bfa50b874d4237461618cf253d.

--- client/src/App.js
+++ client/src/App.js
@@ -2,8 +2,8 @@ import React from 'react';
 import './App.css';
 
 // Components
-import Header from './components/Header/Header';
-import Footer from './components/Footer/Footer';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 // Pages
 import Login from './pages/Login'
@@ -47,13 +47,11 @@ function App() {
               <Route
                 path="/login"
                 element={<Login />}
-                
               />
               <Route
                 path="/signup"
                 element={<Signup />}
               />
-
             </Routes>
           </main>
           <Footer />
