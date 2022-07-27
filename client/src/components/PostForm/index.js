f69d6ca181f6a7c48d849b24ae631073a4346a59
Gerard Mennella <gerardmennella21@gmail.com>
2022-07-27 19:59:05 -0400

    file preview

--- client/src/components/PostForm/index.js
+++ client/src/components/PostForm/index.js
@@ -1,26 +1,94 @@
-import React, {useCallback} from "react";
-import { useDropzone } from "react-dropzone";
+import React, { useCallback, useEffect, useMemo, useState } from 'react';
+import { useDropzone } from 'react-dropzone';
 
-const PostForm = () => {
-    const {acceptedFiles, getRootProps, getInputProps} = useDropzone();
-  
-    const files = acceptedFiles.map(file => (
-        <li key={file.path}>
-        {file.path} - {file.size} bytes
-        </li>
-    ));
+const baseStyle = {
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  padding: '10px',
+  borderWidth: 2,
+  borderRadius: 2,
+  borderColor: '#FFFCF9',
+  borderStyle: 'solid',
+  color: '#FFFCF9',
+  transition: 'border .3s ease-in-out'
+};
+
+const activeStyle = {
+  borderColor: '#2196f3'
+};
+
+const acceptStyle = {
+  borderColor: '#00e676'
+};
+
+const rejectStyle = {
+  borderColor: '#ff1744'
+};
+
+function DropzoneComponent(props) {
+  const [files, setFiles] = useState([]);
+
+  const onDrop = useCallback(acceptedFiles => {
+    setFiles(acceptedFiles.map(file => Object.assign(file, {
+      preview: URL.createObjectURL(file)
+    })));
+  }, []);
+
+  const {
+    getRootProps,
+    getInputProps,
+    isDragActive,
+    isDragAccept,
+    isDragReject
+  } = useDropzone({
+    onDrop,
+    accept: 'image/jpeg, image/png'
+  });
+
+  const style = useMemo(() => ({
+    ...baseStyle,
+    ...(isDragActive ? activeStyle : {}),
+    ...(isDragAccept ? acceptStyle : {}),
+    ...(isDragReject ? rejectStyle : {})
+  }), [
+    isDragActive,
+    isDragReject,
+    isDragAccept
+  ]);
+
+const fileList = files.map(file => (
+    <li key={file.path}>
+      {file.path} - {file.size} bytes
+    </li>
+  ));
+
+  const thumbs = files.map(file => (
+    <div key={file.name}>
+      <img
+        src={file.preview}
+        alt={file.name}
+      />
+    </div>
+  ));
+
+  // clean up
+  useEffect(() => () => {
+    files.forEach(file => URL.revokeObjectURL(file.preview));
+  }, [files]);
 
   return (
-    <section className="container">
-      <div id="Upload" {...getRootProps({className: 'dropzone'})}>
+    <section>
+      <div id='imageUpload' {...getRootProps({style})}>
         <input {...getInputProps()} />
-        <p>Upload An Image</p>
+        <div >Upload An Image</div>
       </div>
       <aside>
-        <ul id="fileChoice">{files}</ul>
+        {thumbs}
+        <ul id='fileList'>{fileList}</ul>
       </aside>
     </section>
-  );
+  )
 }
 
-export default PostForm
+export default DropzoneComponent;
