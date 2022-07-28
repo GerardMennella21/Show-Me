684de67e25f768bd8a93865107a3a1c07fcd3563
Gerard Mennella <gerardmennella21@gmail.com>
2022-07-28 16:51:23 -0400

    modify postform

--- client/src/components/PostForm/index.js
+++ client/src/components/PostForm/index.js
@@ -1,80 +1,80 @@
-import React, {useState } from 'react';
+import React, {useState, useMemo, useCallback, useEffect } from 'react';
 import { useDropzone } from 'react-dropzone';
 import { ADD_IMAGE } from '../../utils/mutations';
 import { useMutation } from '@apollo/client';
 
-// const baseStyle = {
-//   display: 'flex',
-//   flexDirection: 'column',
-//   alignItems: 'center',
-//   padding: '25px',
-//   borderWidth: 2,
-//   borderRadius: 5,
-//   borderColor: '#FFFCF9',
-//   borderStyle: 'solid',
-//   color: '#FFFCF9',
-//   transition: 'border .3s ease-in-out'
-// };
+const baseStyle = {
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  padding: '25px',
+  borderWidth: 2,
+  borderRadius: 5,
+  borderColor: '#FFFCF9',
+  borderStyle: 'solid',
+  color: '#FFFCF9',
+  transition: 'border .3s ease-in-out'
+};
 
-// const activeStyle = {
-//   borderColor: '#2196f3'
-// };
+const activeStyle = {
+  borderColor: '#2196f3'
+};
 
-// const acceptStyle = {
-//   borderColor: '#00e676'
-// };
+const acceptStyle = {
+  borderColor: '#00e676'
+};
 
-// const rejectStyle = {
-//   borderColor: '#ff1744'
-// };
+const rejectStyle = {
+  borderColor: '#ff1744'
+};
 
 function DropzoneComponent(props) {
-  // const [files, setFiles] = useState([]);
+  const [files, setFiles] = useState([]);
 
-  // const onDrop = useCallback(acceptedFiles => {
-  //   setFiles(acceptedFiles.map(file => Object.assign(file, {
-  //     preview: URL.createObjectURL(file)
-  //   })));
-  // }, []);
+  const onDrop = useCallback(acceptedFiles => {
+    setFiles(acceptedFiles.map(file => Object.assign(file, {
+      preview: URL.createObjectURL(file)
+    })));
+  }, []);
 
-  // const {
-  //   getRootProps,
-  //   getInputProps,
-  //   isDragActive,
-  //   isDragAccept,
-  //   isDragReject
-  // } = useDropzone({
-  //   onDrop,
-  //   accept: 'image/jpeg, image/png'
-  // });
+  const {
+    getRootProps,
+    getInputProps,
+    isDragActive,
+    isDragAccept,
+    isDragReject
+  } = useDropzone({
+    onDrop,
+    accept: 'image/jpeg, image/png'
+  });
 
-  // const style = useMemo(() => ({
-  //   ...baseStyle,
-  //   ...(isDragActive ? activeStyle : {}),
-  //   ...(isDragAccept ? acceptStyle : {}),
-  //   ...(isDragReject ? rejectStyle : {})
-  // }), [
-  //   isDragActive,
-  //   isDragReject,
-  //   isDragAccept
-  // ]);
+  const style = useMemo(() => ({
+    ...baseStyle,
+    ...(isDragActive ? activeStyle : {}),
+    ...(isDragAccept ? acceptStyle : {}),
+    ...(isDragReject ? rejectStyle : {})
+  }), [
+    isDragActive,
+    isDragReject,
+    isDragAccept
+  ]);
 
   
 
-  // const fileList = files.map(file => (
-  //   <li key={file.path}>
-  //     {file.path} - {file.size} bytes
-  //   </li>
-  // ));
+  const fileList = files.map(file => (
+    <li key={file.path}>
+      {file.path} - {file.size} bytes
+    </li>
+  ));
 
-  // const thumbs = files.map(file => (
-  //   <div key={file.name}>
-  //     <img
-  //       src={file.preview}
-  //       alt={file.name}
-  //     />
-  //   </div>
-  // ));
+  const thumbs = files.map(file => (
+    <div key={file.name}>
+      <img
+        src={file.preview}
+        alt={file.name}
+      />
+    </div>
+  ));
 
   const handleFormSubmit = async event => {
     event.preventDefault();
@@ -102,27 +102,15 @@ function DropzoneComponent(props) {
 };
 
   // clean up
-  // useEffect(() => () => {
-  //   files.forEach(file => URL.revokeObjectURL(file.preview));
-  // }, [files]);
+  useEffect(() => () => {
+    files.forEach(file => URL.revokeObjectURL(file.preview));
+  }, [files]);
 
-  // console.log(files);
+  console.log(files);
 
   return (
-    <section>
-        <p className={`m-0 ${characterCount === 280 ? 'text-error' : ''}`}>
-            Character Count: {characterCount}/280
-            {error && <span>Something went wrong...</span>}
-        </p>
-      <form onSubmit={handleFormSubmit}>
-        <textarea
-          placeholder="Enter Caption Here"
-          value={imageText}
-          onChange={handleChange}
-        ></textarea>
-        <button type='submit'>Submit</button>
-      </form>
-      {/* <div id='imageUpload' {...getRootProps({style})}>
+    <section>      
+      <div id='imageUpload' {...getRootProps({style})}>
         <input {...getInputProps()} />
         <div id='DZText'>
             Upload An Image <br />
@@ -132,8 +120,20 @@ function DropzoneComponent(props) {
       <aside>
         {thumbs}
         <ul id='fileList'>{fileList}</ul>
-      </aside> */}
-    </section>
+      </aside>
+      <form onSubmit={handleFormSubmit}>
+        <textarea
+          placeholder="Enter Caption Here"
+          value={imageText}
+          onChange={handleChange}
+        ></textarea>
+        <button type='submit'>Submit</button>
+      </form>
+      <p>
+            Character Count: {characterCount}/280
+            {error && <span>Something went wrong...</span>}
+        </p>
+    </section>  
   )
 }
 export default DropzoneComponent;
